4b6c81a4de975c9eabfd420b5ea36296e2a22f70
Aleksandra <155973095+aleksandra-milikic@users.noreply.github.com>
2024-09-07 17:13:56 +0200

    Dodato horizontalno skrolovanje navigacionog menija

--- pokemon-react/pokemonbattlesimulator/src/App.js
+++ pokemon-react/pokemonbattlesimulator/src/App.js
@@ -1,3 +1,4 @@
+// src/App.js
 import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
@@ -11,7 +12,6 @@ import ItemStore from './pages/ItemStore';
 import BattleRules from './pages/BattleRules';
 import './App.css';
 
-
 const App = () => {
 
   useEffect(() => {
@@ -23,52 +23,32 @@ const App = () => {
     <Router>
       <nav>
         <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-          <li>
-            <Link to="/battle">Battle</Link>
-          </li>
-          <li>
-            <Link to="/rankingtable">RankingTable</Link>
-          </li>
-          <li>
-            <Link to="/userprofile">UserProfile</Link>
-          </li>
-          <li>
-            <Link to="/pokemon-list">PokemonList</Link>
-          </li>
-          <li>
-            <Link to="/itemstore">ItemStore</Link>
-          </li>
-          <li>
-            <Link to="/battlerules">BattleRules</Link>
-          </li>
-          <li>
-            <Link to="/themesettings">ThemeSettings</Link>
-          </li>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/login">Login</Link></li>
+          <li><Link to="/battle">Battle</Link></li>
+          <li><Link to="/rankingtable">RankingTable</Link></li>
+          <li><Link to="/userprofile">UserProfile</Link></li>
+          <li><Link to="/pokemon-list">PokemonList</Link></li>
+          <li><Link to="/itemstore">ItemStore</Link></li>
+          <li><Link to="/battlerules">BattleRules</Link></li>
+          <li><Link to="/themesettings">ThemeSettings</Link></li>
         </ul>
       </nav>
-
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/battle" element={<BattlePage />} />
-        <Route path="/rankingtable" element={<RankingTable />} />
-        <Route path="/userprofile" element={<UserProfile />} />
-        <Route path="/pokemon-list" element={<PokemonList />} />
-        <Route path="/itemstore" element={<ItemStore />} />
-        <Route path="/battlerules" element={<BattleRules />} />
-        <Route path="/themesettings" element={<ThemeSettings />} />
-      </Routes>
+      <main>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/battle" element={<BattlePage />} />
+          <Route path="/rankingtable" element={<RankingTable />} />
+          <Route path="/userprofile" element={<UserProfile />} />
+          <Route path="/pokemon-list" element={<PokemonList />} />
+          <Route path="/itemstore" element={<ItemStore />} />
+          <Route path="/battlerules" element={<BattleRules />} />
+          <Route path="/themesettings" element={<ThemeSettings />} />
+        </Routes>
+      </main>
     </Router>
   );
 };
 
 export default App;
-
-
-
